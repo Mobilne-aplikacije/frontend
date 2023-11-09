6143b934126495c83110fe089443683a9acf6e23
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-11-09 11:45:58 +0100

    Update accommodation details for guest and host

--- app/src/main/java/com/example/uberapp_tim22/AccomodationHostActivity.java
+++ app/src/main/java/com/example/uberapp_tim22/AccomodationHostActivity.java
@@ -1,14 +1,106 @@
 package com.example.uberapp_tim22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AccomodationHostActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accomodation_host);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Booking");
+
+        sharedPreferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        Long myId = sharedPreferences.getLong("pref_id", 0);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.item1){
+            Intent intent = new Intent(this, DriverAccountActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item6){
+            Intent intent = new Intent(this, AccomodationScreen.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item7){
+            Intent intent = new Intent(this, HostReservationScreen.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item8){
+            Intent intent = new Intent(this, NotificationDriverScreen.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item2){
+            Intent intent = new Intent(this, DriverInboxActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item3){
+            Intent intent = new Intent(this, DriverRideHistory.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item4){
+            Intent intent = new Intent(this, DriverMainActivity.class);
+            startActivity(intent);
+        }
+        if(id == R.id.item5){
+            Intent intent = new Intent(this, UserLoginActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart()", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop()",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy()",Toast.LENGTH_SHORT).show();
     }
 }
